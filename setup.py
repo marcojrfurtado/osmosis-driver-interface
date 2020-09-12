c536e1d35730228f9888ee0f5202613f9364ca2f
Antonio Carlos Furtado <marcojrfurtado@gmail.com>
2020-09-12 19:09:07 -0400

    Bump version: 0.0.8 → 0.0.9

--- setup.py
+++ setup.py
@@ -71,6 +71,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/marcojrfurtado/osmosis-driver-interface-plus',
-    version='0.0.8',
+    version='0.0.9',
     zip_safe=False,
 )
